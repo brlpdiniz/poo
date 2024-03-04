1a8309b7cd659b0383be3344e000ab44684112a5
brlpdiniz <brunoldiniz14@gmail.com>
2024-03-04 20:18:08 -0300

    updt: Aula 1

--- Aulas/aula1.py
+++ Aulas/aula1.py
@@ -1,11 +1,10 @@
+import math
+
 a = 'Bruno'
 b = "Bruno"
 c = 25
 d = True
 print(a,b,c,d)
-
-
-
 
 # 2.6 Exercícios: Variáveis
 # 1) Escreva mais 3 identificadores que não possam ser usados como nome de variáveis.
@@ -14,14 +13,14 @@ print(a,b,c,d)
     # 2Variavel3
 
 # 2) Experimente as funções de conversão de base.
-# nome = input("Digite seu nome: ")
-# idade = int(input("Digite a sua idade: "))
-# altura = float(input("Digite a sua altura: "))
-# print(nome, idade, altura)
+nome = input("Digite seu nome: ")
+idade = int(input("Digite a sua idade: "))
+altura = float(input("Digite a sua altura: "))
+print(nome, idade, altura)
 
-# print(hex(idade))
-# print(bin(idade))
-# print(oct(idade))
+print(hex(idade))
+print(bin(idade))
+print(oct(idade))
 
 # 3) Experimente também a forma de formatação de string para converter números entre diferentes bases.
 x = 255
@@ -49,15 +48,32 @@ print(a2.capitalize())
 
 
 # 3.4 Exercícios: Strings
-# 1) Considere a string A = "Um elefante incomoda muita gente". Que fatia corresponde a "elefante incomoda"?
+# a) Considere a string A = "Um elefante incomoda muita gente". Que fatia corresponde a "elefante incomoda"?
 e = "Um elefante incomoda muita gente"
 print(e[3:20])
 
-# 2) Escreva um programa que solicite uma frase ao usuário e reescreva a frase toda em maiúscula e sem espaços em branco.
+# b) Escreva um programa que solicite uma frase ao usuário e reescreva a frase toda em maiúscula e sem espaços em branco.
 frase = input("Digite uma frase: ")
 print(frase.upper().replace(" ", ""))
 
-# 3) Escreva um programa que solicite uma frase ao usuário e reescreva a frase usando “leet speak”. Por exemplo, hello -> h3110, leet -> 1337, gaming -> g4m1ng, password -> p455w0rd. Utilize a função replace neste caso.
+# c) Escreva um programa que solicite uma frase ao usuário e reescreva a frase usando “leet speak”. Por exemplo, hello -> h3110, leet -> 1337, gaming -> g4m1ng, password -> p455w0rd. Utilize a função replace neste caso.
 
-frase2 = input("Digite uma frase: ")
-print(frase2.replace("", ""))
+frase2 = (input("Digite uma frase: "))
+frase2 = frase2.upper()
+frase2.replace("a", "4")
+frase2.replace("e", "3")
+frase2.replace("i", "1")
+frase2.replace("l", "1")
+frase2.replace("o", "0")
+frase2.replace("s", "5")
+
+# 4.2 Escreva um programa que receba 2 valores do tipo inteiro x e y, e calcule o valor de z:
+#     (x² + y² )
+# z = __________
+#     ( x − y )²
+
+w = int(input("Digite o valor de W: "))
+z = int(input("Digite o valor de Z: "))
+
+result = (w*w+z*z)/((w-z)**2)
+print(result)
