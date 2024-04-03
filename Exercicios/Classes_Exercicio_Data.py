94d483d28a89c9124b113103101b0630dd31c712
brlpdiniz <brunoldiniz14@gmail.com>
2024-04-03 18:09:06 -0300

    updt: Classes_Exercicio_Data

--- Exercicios/Classes_Exercicio_Data.py
+++ Exercicios/Classes_Exercicio_Data.py
@@ -6,20 +6,18 @@ class Data:
         
     # isBissexto():bool
     def isBissexto(self) -> bool:
-        if (self.ano % 4 == 0 and self.ano % 100 != 0 ) or (self.ano % 400 == 0):
-            return True
-        else:
-            return False
+        if (self.ano)
 
+
+        return
+    
     # trimestre():int
-    def trimestre(self) -> int:
-        
-            return 
+    def trimestre() -> int:
+        return 
     
     # numDiasMes():int
     def numDiasMes(self) -> int:
-        if (self.mes == 1 or 3 or 5 or 7 or 8 or 10 or 12):
-            return
+        return
     
     #dataValida():bool
     def dataValida(self) -> bool:
